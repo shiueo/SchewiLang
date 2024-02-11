bdbc5f1c11608cac324834f39687c3f7b837602b
shiüo <shiueo.csh@gmail.com>
2024-02-12 01:11:19 +0900

    d

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import json
 with open("KrToSchewiUni.json", "r", encoding="utf8") as f:
     data = json.load(f)
 
-test_str = "즈에 이이카 아이로 오넨네시다네"
+test_str = "단단 오치테쿠 아이큐우 쿠소와로 / 칸젠타이노 야마다 / 무칸신난테 아리에나이 / 산젠타이노 쿠와타노 호오가 이이카나 / 와가마마바카리자 오치츠카나이"
 
 res = ""
 for word in test_str:
